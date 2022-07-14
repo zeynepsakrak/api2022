f40cb72c33041d47a080013a2a31784128c13a00
zeynepsakrak <100724081+zeynepsakrak@users.noreply.github.com>
2022-07-14 14:55:34 +0300

    07/13

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -3,10 +3,8 @@ package get_requests;
 import base_urls.HerOkuAppBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.assertEquals;
 
@@ -37,7 +35,6 @@ public class Get09 extends HerOkuAppBaseUrl {
         spec.pathParams("first","booking","second",91);
 
         //2. Step: Set the expected data
-
         Map<String, String> bookingdatesMap = new HashMap<>();
         bookingdatesMap.put("checkin","2018-01-01");
         bookingdatesMap.put("checkout","2019-01-01");
@@ -60,7 +57,11 @@ public class Get09 extends HerOkuAppBaseUrl {
         assertEquals(expectedDataMap.get("firstname"),actualDataMap.get("firstname"));
         assertEquals(expectedDataMap.get("lastname"),actualDataMap.get("lastname"));
         assertEquals(expectedDataMap.get("depositpaid"),actualDataMap.get("depositpaid"));
+        assertEquals(expectedDataMap.get("totalprice"),actualDataMap.get("totalprice"));
+        assertEquals(expectedDataMap.get("additionalneeds"),actualDataMap.get("additionalneeds"));
         assertEquals(bookingdatesMap.get("checkin"),((Map)actualDataMap.get("bookingdates")).get("checkin"));
         assertEquals(bookingdatesMap.get("checkout"),((Map)actualDataMap.get("bookingdates")).get("checkout"));
+
     }
+
 }
